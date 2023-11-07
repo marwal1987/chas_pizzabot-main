8aec60357600d057f9ae96011a8a9b938a3ab84d
marwal1987 <marwal1987@gmail.com>
2023-11-07 11:43:43 +0100

    added while loops

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,35 +1,39 @@
 // Check prompt vs pizza menu
-function checkOrderName(orderName) {
-  //  Add while loop to restart when alert i triggered in else?
+function checkOrderName(order) {
   if (
-    orderName.toLowerCase() == vegetarian.toLowerCase() ||
-    orderName.toLowerCase() == hawaiian.toLowerCase() ||
-    orderName.toLowerCase() == pepperoni.toLowerCase()
+    order.trim().toLowerCase() == vegetarian.toLowerCase() ||
+    order.trim().toLowerCase() == hawaiian.toLowerCase() ||
+    order.trim().toLowerCase() == pepperoni.toLowerCase()
   ) {
-    return orderName;
+    return true;
   } else {
     alert(
-      `Sorry, ${orderName} is not on the menu. Please select a pizza from the menu: "Vegetarian Pizza", "Hawaiian Pizza" or "Pepperoni Pizza"`
+      `Sorry, ${order} is not on the menu. Please select a pizza from the menu: "Vegetarian Pizza", "Hawaiian Pizza" or "Pepperoni Pizza"`
     );
+    return false;
+  }
+}
+
+// Calculate cooking time
+function cookingTime(order) {
+  if (order >= 6 && order <= 10) {
+    waitTime = "The pizzas will take 20 minutes.";
+    return true;
+  } else if (order <= 5 && order >= 3) {
+    waitTime = "The pizzas will take 15 minutes.";
+    return true;
+  } else if (order <= 2 && order >= 1) {
+    waitTime = "The pizzas will take 10 minutes.";
+    return true;
+  } else {
+    alert(`You can't order ${order} ${orderName}s. Min: 1 Max: 10`);
+    return false;
   }
 }
 
 // Calculate total cost
 function totalCost(order) {
   return (orderTotal = order * pizzaPrice);
-}
-
-// Calculate cooking time
-function cookingTime(orderQuantity) {
-  if (orderQuantity >= 6) {
-    cookingTime = "The pizzas will take 20 minutes.";
-  } else if (orderQuantity <= 5 && orderQuantity >= 3) {
-    cookingTime = "The pizzas will take 15 minutes.";
-  } else if (orderQuantity <= 2 && orderQuantity >= 1) {
-    cookingTime = "The pizzas will take 10 minutes.";
-  } else {
-    return alert(`You can't order 0 ${orderName}`);
-  }
 }
 
 // The menu and price
@@ -47,17 +51,30 @@ alert(
 // Ask the user which pizza they want, and how many
 let orderName = prompt("Enter the name of the pizza you want to order today.");
 
+while (!checkOrderName(orderName)) {
+  orderName = prompt("Enter the name of the pizza you want to order today.");
+}
 checkOrderName(orderName);
 
 let orderQuantity = prompt(`How many of ${orderName} do you want?`);
 
 // Finalizing the order
-
+let waitTime;
 let orderTotal;
-totalCost(orderQuantity);
+
+/* NOTE TO SELF:
+totalCost(orderQuantity); // Placing the call here created an issue. If the user choose to put an invalid number in the orderQuantity prompt,
+it still got saved in the variable... So the totalCost went off the charts even if you in the end only ordered 2 pizzas after being prompted so.
+After placing the function call after the cookingTime loop and Call, this cured the problem.  
+*/
+
+while (!cookingTime(orderQuantity)) {
+  orderQuantity = prompt(`How many of ${orderName} do you want?`);
+}
 
 cookingTime(orderQuantity);
+totalCost(orderQuantity);
 
 alert(
-  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. ${cookingTime}`
+  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. ${waitTime}`
 );
